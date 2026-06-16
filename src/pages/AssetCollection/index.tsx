70f124aaf39c3a349d70115c852fa0970d7261b6
Gustavo <gustavoonishinl@gmail.com>
2026-06-16 16:01:27 -0300

    better audio player

--- src/pages/AssetCollection/index.tsx
+++ src/pages/AssetCollection/index.tsx
@@ -1,7 +1,11 @@
 import type { JSX } from 'react';
 import GoBackButton from '../../components/GoBackButton';
 
-const assetRelativePath : string = import.meta.env.VITE_ASSETS_RELATIVE_PATH ?? '';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+const assetRelativePath: string =
+    import.meta.env.VITE_ASSETS_RELATIVE_PATH ?? '';
 
 const AssetCollection: React.FC<{
     parentLabel: string;
@@ -16,25 +20,20 @@ const AssetCollection: React.FC<{
             <h1>
                 {parentLabel} - {label}
             </h1>
-            {assets
-                .map(({ label, file }) => {
-                    const filePath = assetRelativePath + folder + file;
-                    let item: JSX.Element | null = null;
-                    if (file.endsWith('.mp3')) {
-                        // Embed mp3 player
-                        item = (
-                            <audio key={file} controls>
-                                <source src={filePath} type='audio/mp3' />
-                            </audio>
-                        );
-                    }
-                    return (
-                        <div key={file}>
-                            <h4>{label}</h4>
-                            {item ?? 'Arquivo não identificado'}
-                        </div>
-                    );
-                })}
+            {assets.map(({ label, file }) => {
+                const filePath = assetRelativePath + folder + file;
+                let item: JSX.Element | null = null;
+                if (file.endsWith('.mp3')) {
+                    // Embed mp3 player
+                    item = <AudioPlayer key={file} src={filePath} />;
+                }
+                return (
+                    <div key={file}>
+                        <h4>{label}</h4>
+                        {item ?? 'Arquivo não identificado'}
+                    </div>
+                );
+            })}
         </div>
     );
 };
