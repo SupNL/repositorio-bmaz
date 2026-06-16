c2efcdfdf87a2b56d08842939275d4a19b2630fc
Gustavo <gustavoonishinl@gmail.com>
2026-06-16 18:47:52 -0300

    bump build

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const version = 'version 2026061601';
+console.log(version);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
